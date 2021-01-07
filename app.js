77e061fdf8025ab146f84910962bd2889b20f69b
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-07 14:56:46 -0800

    add calculator with 4 operations with simple style

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
-// import functions and grab DOM elements
+const operationField = document.getElementById('operation-input-1');
+const operationField2 = document.getElementById('operation-input-2');
 
-// initialize state
+const addButton = document.getElementById('addition-button');
+const subButton = document.getElementById('subtraction-button');
+const multButton = document.getElementById('multiplication-button');
+const divButton = document.getElementById('devision-button');
 
-// set event listeners to update state and DOM
+const operationResults = document.getElementById('operation-results');
+
+
+function showResult(result) {
+    operationResults.textContent = "RESULT: " + result;
+}
+
+addButton.addEventListener('click', () => {
+    const fieldValue1 = operationField.value;
+    const fieldValue2 = operationField2.value;
+    
+    showResult(Number(fieldValue1) + Number(fieldValue2));
+
+});
+
+subButton.addEventListener('click', () => {
+    const fieldValue1 = operationField.value;
+    const fieldValue2 = operationField2.value;
+    
+    showResult(Number(fieldValue1) - Number(fieldValue2));
+
+});
+
+multButton.addEventListener('click', () => {
+    const fieldValue1 = operationField.value;
+    const fieldValue2 = operationField2.value;
+    
+    showResult(Number(fieldValue1) * Number(fieldValue2));
+
+});
+
+divButton.addEventListener('click', () => {
+    const fieldValue1 = operationField.value;
+    const fieldValue2 = operationField2.value;
+    
+    showResult(Number(fieldValue1) / Number(fieldValue2));
+
+});
